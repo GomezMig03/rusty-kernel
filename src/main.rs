d4d867efca586b7c1a312f72164c7833581bded4
gomezmig03 <gomezmig03@gmail.com>
2024-09-18 13:52:25 +0200

    Implement basic VGA buffer interface

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,13 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buff = 0xb8000 as *mut u8; // Raw pointer with the VGA buffer address
-
-    // Iterate the "Hello" word and write it in the VGA adresses with cyan color (0xb) 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buff.offset(i as isize * 2) = byte;
-            *vga_buff.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
