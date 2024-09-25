5c925d4e765fcd91d27cba02ddf827b0f7853682
gomezmig03 <gomezmig03@gmail.com>
2024-09-25 12:45:27 +0200

    Separate lib logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rusty_kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-use core::panic::PanicInfo;
 
-mod vga_buffer;
-mod serial;
+use core::panic::PanicInfo;
+use rusty_kernel::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -26,54 +25,11 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Panics and prints the error to the host machine, made for tests only.
+
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+    rusty_kernel::test_panic_handler(info)
 }
 
 #[test_case]
